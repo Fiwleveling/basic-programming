743cc8c8a8b9bfe5b746990c2cb665a2c64d1ecb
Fiwleveling <mnaswul@gmail.com>
2024-07-19 16:23:09 +0700

    ส่งงาน

--- ep1/index.ts
+++ ep1/index.ts
@@ -1,7 +1,15 @@
-const firstName: string = 'manasavee'     
+const firstName: string = 'มนัสวี'     
 let y: number = 10
 y =20
 let อยากกลับบ้าน: boolean = true 
+
 console.log(firstName)
 console.log(y)
 console.log(อยากกลับบ้าน)
+
+
+const a: number = 5
+const b: number = 10
+console.log(a!==b)
+console.log(a>=b)
+console.log(a<=b)
