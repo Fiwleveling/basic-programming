1406eecc5e30576fb6d6c827f98dd4d20b5e7571
Fiwleveling <mnaswul@gmail.com>
2024-08-23 16:25:19 +0700

    loop

---
+++ ep6/index.ts
@@ -0,0 +1,99 @@
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log(6)
+console.log(7)
+console.log(8)
+console.log(9)
+console.log(10)
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
+
+//let sum: number = 0
+
+//for(let i = 1; i <= 31; i + 5) {
+    //sum +sum + i
+//}
+
+//console.log('Total: ', sum)
+
+//let sum: number = 0
+//let i: number =1
+
+//while (i <= 31) {
+    //sum = sum + i
+    //i++
+//}
+
+//console.log('Total: ', sum)
+
+// let sum: number = 0
+// let i: number = 1
+
+// do {
+//     sum = sum + 1
+//     i++
+// } while (i <= 31)
+
+// console.log('Total: ', sum)
+
+// let sum: number = 0
+// let i: number = 1
+
+// while (i <= 31) {
+//     sum = sum + 1
+//     i++
+// }
+
+// console.log('Total: ', sum)
+
+// let count: number = 0
+
+// while (true) {
+//     console.log ("โสดโว้ย")
+//     count++
+//     if(count === 10) {
+//         break
+//     }
+// }
+
+// let count: number = 0
+
+// while (true) {
+//     console.log ("อายุ 18")
+//     count++
+//     if(count === 18) {
+//         break
+//     }
+// }
+
+// for(let i = 1; i <= 18; i ++) {
+//     console.log('ฉันอายุ ' + i + ' ปี')
+// }
+
+// let car1: string = 'Toyota'
+// let car2: string = 'Honda'
+// let car3: string = 'Suzuki'
+// let car4: string = 'Yamaha'
+// let car5: string = 'Mazda'
+
+// const cars: string[] = ['Toyota','Honda','Suzuki','Yamaha','Mazda']
+// const num: number[] = [1,2,3,4,5]
+// const bool: boolean[] = [true,true,false,false]
+
+// const cars: string[] = ['Toyota','Honda','Suzuki','Yamaha','Mazda']
+
+// console.log(cars.length)
+
+// const amountNum: number[] =[1,2,3,4,5,6,7,8,9,10]
+
+// for(let i = 0; i <= amountNum.length; i ++) {
+//     if (amountNum[i] === 3 || amountNum[i] === 5 || amountNum[i] === 7) {
+//        amountNum[i]= 0
+//     }
+// }
+//  console.log(amountNum)
