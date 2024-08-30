5bbc9483b53d67d2753c1bd66629c9b55700e5c6
Fiwleveling <mnaswul@gmail.com>
2024-08-30 16:12:00 +0700

    การบ้าน

---
+++ ep7/index.ts
@@ -0,0 +1,109 @@
+// const score = [10,23,26,30,33]
+
+// let sum: number = 0
+
+// for (let i = 0; i < score.length; i++) {
+//     sum = sum + score[i]
+// }
+
+// let avg = sum / score.length
+// console.log("Total: ", sum)
+// console.log("Avg", avg)
+
+
+// const myStudents: string = "เซน,เเทน,เวฟ,รุ้ง,เท็ก,สร"
+
+// const myStudentsArray: string[] =myStudents.split(',')
+
+// console.log (myStudents)
+// console.log (myStudentsArray)
+
+
+// const fullName: string = 'สุดสวย สวยสุด'
+
+// console.log(fullName.split(' ')[0])
+
+//console.log(fullName.slice(0, fullName.lastIndexOf(' ')))
+
+
+// const aniMal: string = "เเมว*หมา*นก*กระรอก"
+
+// const aniMalArray: string[] =aniMal.split('*')
+
+// console.log(aniMal)
+// console.log("ตั้งต้น: ",aniMalArray)
+
+// aniMalArray.pop() //เอาตัวสุดท้ายออก
+
+// console.log("เอาตัวสุดท้ายออก: ", aniMalArray)
+
+// aniMalArray.push('ยีราฟ') //ใส่ต่อท้าย
+
+// console.log("ใส่ต่อท้าย: ", aniMalArray)
+
+// aniMalArray.shift() //เอาตัวหน้าออก
+
+// console.log("เอาตัวหน้าออก: ",aniMalArray)
+
+// aniMalArray.unshift('นกฮูก') //ใส่ตัวหน้า
+
+// console.log("ใส่ตัวหน้า: ", aniMalArray)
+
+
+// // const fruits = ["Banana","Orange","Apple","Mango"]
+
+// // fruits.sort() //เรียง A - Z 
+
+// // console.log("เรียง A-Z: ", fruits)
+
+// // fruits.reverse() //เรียง Z - A 
+
+// // console.log("เรียง Z-A: ", fruits)
+
+
+// const friendLIike: string = "Nam,Ink,T,Boss"
+
+// const friendLIikeArray: string[] =friendLIike.split(',')
+
+// console.log(friendLIike)
+// console.log(friendLIikeArray)
+
+// friendLIikeArray.sort()
+
+// console.log(friendLIikeArray)
+
+// friendLIikeArray.reverse()
+
+// console.log(friendLIikeArray)
+
+
+const me = {
+    firstName: 'Gojoo',
+    lastName: 'Satoru',
+    age: 18 ,
+    nickName: 'Gojo' ,
+    gender: 'XX' ,
+    phone: '1122' ,
+    address: '00' ,
+    village: '-' ,
+    road: '-' ,
+    province: 'Shinjuku' ,
+    district: 'Tokyo' ,
+    zipcode: '102xxx' ,
+    gmail: 'abcdfg@gmail.com'
+ 
+}
+
+// ประวัติส่วนตัว
+// ชื่อ: Gojoo
+// นามสกุล: Satoru
+// อายุ 26
+// ชื่อเล่น Gojo
+// เพศ xx
+// เบอร์ 1122
+// เลขที่บ้าน 00
+// หมู่บ้าน -
+// ถนน -
+// จังหวัด ชินจูกุ
+// เขต ชินจูกุ
+// อีเมล  abcdfg@gmail.com
